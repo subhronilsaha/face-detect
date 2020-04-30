0b9cb48fcad93195254b0bd27da9aac32b58419a
Subhronil Saha <subhronilsaha@Subhronils-MacBook-Air.local>
2020-04-30 11:00:18 +0530

    Updated

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -6,7 +6,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div className="text-light mt-5">
             <p style={{fontSize: "20px"}}>This software will detect faces in your pictures! (powered by Clarif.ai)</p>
-            <p style={{fontSize: "20px"}} className="text-success">Enter a link to an image to begin.</p>
+            <p style={{fontSize: "20px"}} className="text-success">Enter a link to an image to begin. (Portrait images preferred for maximum accuracy)</p>
             <div>
                 <input className="image-link-form" type="text" onChange={ onInputChange } />
                 <Button variant="success" onClick={ onButtonSubmit }>Detect Faces</Button>
