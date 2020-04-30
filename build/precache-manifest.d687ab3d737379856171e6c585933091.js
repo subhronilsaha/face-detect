042dbb7cd13bfa07c1c48e65ec6d18fb8b85d24d
Subhronil Saha <subhronilsaha@Subhronils-MacBook-Air.local>
2020-04-30 10:22:45 +0530

    Updated

---
+++ build/precache-manifest.d687ab3d737379856171e6c585933091.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dadd289040cd8026080c7164a027794",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09dcae61fdf8a371ef51",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "49c836e5cf50fd57db98",
+    "url": "/static/css/main.c30bd70d.chunk.css"
+  },
+  {
+    "revision": "09dcae61fdf8a371ef51",
+    "url": "/static/js/2.f477428c.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/static/js/2.f477428c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49c836e5cf50fd57db98",
+    "url": "/static/js/main.27251385.chunk.js"
+  },
+  {
+    "revision": "0fe048ca11a67524e742",
+    "url": "/static/js/runtime-main.9d2925b7.js"
+  },
+  {
+    "revision": "e9b8780250559653ad9d06957ce289f6",
+    "url": "/static/media/Logo.e9b87802.png"
+  },
+  {
+    "revision": "b42f7c0b0484a6f605212a35093e4743",
+    "url": "/static/media/bg-img.b42f7c0b.jpg"
+  }
+]);
